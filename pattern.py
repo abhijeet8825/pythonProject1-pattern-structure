f0c65d607375b5f92389f07744d47ea3e3504c74
ABHIJEET KUMAR <abhijeet8825>
2021-05-27 17:00:07 +0530

    printing pattern

---
+++ pattern.py
@@ -0,0 +1,19 @@
+x=input('Enter True or False\n')
+
+def pattern(n):
+    if x=='t':
+        for i in range(n):
+            for j in range(i+1):
+                print('*',end='')
+
+            print()
+
+    else:
+        for i in range(n):
+            for j in range(n-i):
+                print('*',end='')
+
+            print()
+pattern(5)
+
+
